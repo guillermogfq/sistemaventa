0b187215cc9c8b8122750684cc202d728be217f5
jaime <jaime_ibarra_64@hotmail.com>
2019-02-08 21:00:01 -0300

    cambios de jibarra

--- src/cl/milprogramadores/venta/modelos/DetalleVenta.java
+++ src/cl/milprogramadores/venta/modelos/DetalleVenta.java
@@ -5,18 +5,73 @@
  */
 package cl.milprogramadores.venta.modelos;
 
+import cl.milprogramadores.venta.coneccion.Conexion;
+import java.sql.Connection;
+
 /**
  *
  * @author guillermofuentesquijada
  */
 public class DetalleVenta {
     //Constantes
-    public static final String NOMBRE_TABLA = "ventaproducto";
-    public static final String CLAVE_PRIMARIA = "idventaproducto";
+    public static final String NOMBRE_TABLA = "productoventa";
+    public static final String CLAVE_PRIMARIA = "idproductoventa";
     
     //Atributos
+    private Integer idproductoventa;
+    private Integer cantidad;
+    private Producto idproducto;
+    private Venta idventa;
+
+    public DetalleVenta(Integer idproductoventa, Integer cantidad, Producto idproducto, Venta idventa) {
+        this.idproductoventa = idproductoventa;
+        this.cantidad = cantidad;
+        this.idproducto = idproducto;
+        this.idventa = idventa;
+    }
+
+    public Integer getIdproductoventa() {
+        return idproductoventa;
+    }
+
+    public void setIdproductoventa(Integer idproductoventa) {
+        this.idproductoventa = idproductoventa;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(Producto idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public Venta getIdventa() {
+        return idventa;
+    }
+
+    public void setIdventa(Venta idventa) {
+        this.idventa = idventa;
+    }
+
+   
     
     public boolean guardar(){
+        boolean resultado = true;
+        Connection conn = Conexion.getConexion().getConn();
+        if(this.idproductoventa != null){
+            String query = "UPDATE " + NOMBRE_TABLA + "SET idproducto = ?, idventa = ?, cantidad = ? WHERE " + CLAVE_PRIMARIA + " = ?"; 
+        }else{
+            
+        }
         return false;
     }
     
